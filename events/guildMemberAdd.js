e13b72051a04c375b5d813b6dae399b35a2441d6
MAHOfficial <admin@mahofficial.win>
2018-12-24 17:24:02 +0500

    Updated Welcome Message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,9 @@ module.exports = (client, member) => {
 
   if (settings.welcomeEnabled !== "true") return;
 
-  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", `${member}`);
+  const welcomeMessage = settings.welcomeMessage.replace("{{server}}", `${member.guild.name}`);
+  const welcomeMessage = settings.welcomeMessage.replace("{{memberCount}}", `${member.guild.memberCount}`);
 
   member.guild.channels.find(c => c.name === settings.welcomeChannel).send(welcomeMessage).catch(console.error);
 };
