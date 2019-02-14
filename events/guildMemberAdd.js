3652994cd3e4dd9e8327bbef655dbff9ccda1056
ranakhas <33785157+ranakhas@users.noreply.github.com>
2019-02-14 16:36:14 +0500

    Add Image Welcome System

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,20 +1,33 @@
 const Discord = require("discord.js");
-module.exports = (client, member) => {
+module.exports = async (client, member) => {
   const settings = client.getSettings(member.guild.id);
 
-  if (settings.welcomeEnabled !== "true") return;
-
-const welcomeMessage1 = settings.welcomeMessage.replace("!user", `${member}`);
+    if (settings.welcomeEnabled !== "true") return;
+	    const channel = member.guild.channels.find("name", settings.welcomeChannel);
+		    if (!channel) return;
+  if (settings.WelcomeType !== "text") {
+	 const welcomeMessage1 = settings.welcomeMessage.replace("!user", `${member}`);
 const welcomeMessage2 = welcomeMessage1.replace("!server", `${member.guild.name}`);
 const welcomeMessage3 = welcomeMessage2.replace("!memberCount", `${member.guild.memberCount}`);
- let welcomembed = new Discord.RichEmbed()
+let welcomembed = new Discord.RichEmbed()
         .setTitle("A User Joined")
   .setColor(4886754)
   .setDescription(welcomeMessage3)
   .setFooter("ID: " + member.user.id, member.user.avatarURL)
   .setThumbnail(member.user.avatarURL)
   .setTimestamp()
-  .addField("Full Username:", member.user.tag)
+  .addField("Full Username:", member.user.tag);
+        channel.send(welcomembed).catch(console.error);
+  }
+      if (settings.WelcomeType === "image") {
+		  let welcomeimage = await client.API.welcome("anime", member.user.bot, member.user.avatarURL, member.user.tag, member.user.tag,`${member.guild.name}#${member.guild.memberCount}`);
+        channel.send({
+          files: [{
+            attachment: welcomeimage,
+            name: "welcome.png"
+          }]
+        }).catch(console.error);
 
-  member.guild.channels.find(c => c.name === settings.welcomeChannel).send(welcomembed).catch(console.error);
+}
+
 };
