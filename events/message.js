288dfc462e88c97f8ae6d93eb8e0233484f32e34
ranakhas <33785157+ranakhas@users.noreply.github.com>
2019-02-14 16:37:09 +0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,10 +1,31 @@
 const Discord = require('discord.js');
-const db = require('quick.db');
 
 module.exports = async (client, message) => {
   if (message.author.bot) return;
 
-
+let score;
+  if (message.guild) {
+    score = client.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
+    if(score.level < curLevel) {
+      score.level++;
+      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+    }
+    client.setScore.run(score);
+  }
+  if (!score) {
+  score = {
+    id: `${message.guild.id}-${message.author.id}`,
+    user: message.author.id,
+    guild: message.guild.id,
+    points: 0,
+    level: 1
+  }
+}
   const settings = message.settings = client.getSettings(message.guild.id);
 
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
